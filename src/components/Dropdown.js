455f0d1461920f5d5b6f350ad9c48778dd3f64e5
jeremybivans <jeremybivans@gmail.com>
2023-10-17 15:32:08 -0600

    Employee Availability w/ Dropdown menus

---
+++ src/components/Dropdown.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './Dropdown.css';
+import './EmpAvail.css';
+
+// Status Dropdown Component
+export function StatusDropdown() {
+  const [selectedStatus, setSelectedStatus] = useState('Select Status');
+
+  return (
+    <div className="dropdown">
+      <button className="dropbtn">{selectedStatus}</button>
+      <div className="dropdown-content">
+        <select onChange={(e) => setSelectedStatus(e.target.value)}>
+          <option value="Open">Open</option>
+          <option value="Off">Off</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+// Time Dropdown Component
+export function TimeDropdown() {
+  const [selectedTime, setSelectedTime] = useState('Select Time');
+  const timeIncrements = [];
+
+  for (let i = 0; i < 24; i++) {
+    for (let j = 0; j < 60; j += 30) {
+      const hours = i.toString().padStart(2, '0');
+      const minutes = j.toString().padStart(2, '0');
+      timeIncrements.push(`${hours}:${minutes}`);
+    }
+  }
+
+  return (
+    <div className="dropdown">
+      <button className="dropbtn">{selectedTime}</button>
+      <div className="dropdown-content">
+        <select onChange={(e) => setSelectedTime(e.target.value)}>
+          {timeIncrements.map(time => (
+            <option key={time} value={time}>{time}</option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/EmpAvail.js
@@ -0,0 +1,72 @@
+// EmpAvail.js
+import React from 'react';
+import styles from './EmpAvail.css';
+import { StatusDropdown, TimeDropdown} from './Dropdown';
+
+function EmpAvail() {
+    const numItems = 32;
+  return (
+
+     <div className="emp-avail">
+      {/* <div  style={{ position: 'fixed', left: 350, width: 750, height: 130, background: '#E4E4E4', border: '2px black solid'}} />      Add more content as needed */}
+      {/* <h1 className={styles.window}>Employee Availability</h1> */}
+       <div class="flex-container">
+         <div class="flex-item"></div>
+         <div class="flex-item">Monday</div>
+         <div class="flex-item">Tuesday</div>
+         <div class="flex-item">Wednesday</div>
+         <div class="flex-item">Thursday</div>
+         <div class="flex-item">Friday</div>
+         <div class="flex-item">Saturday</div>
+         <div class="flex-item">Sunday</div>
+         <div class="flex-item">Availability</div>
+         <div class="flex-item"><StatusDropdown /></div>
+         <div class="flex-item"><StatusDropdown /></div>
+         <div class="flex-item"><StatusDropdown /></div>
+         <div class="flex-item"><StatusDropdown /></div>
+         <div class="flex-item"><StatusDropdown /></div>
+         <div class="flex-item"><StatusDropdown /></div>
+         <div class="flex-item"><StatusDropdown /></div>
+         <div class="flex-item">Start</div>
+         <div class="flex-item"><TimeDropdown /></div>
+         <div class="flex-item"><TimeDropdown /></div>
+         <div class="flex-item"><TimeDropdown /></div>
+         <div class="flex-item"><TimeDropdown /></div>
+         <div class="flex-item"><TimeDropdown /></div>
+         <div class="flex-item"><TimeDropdown /></div>
+         <div class="flex-item"><TimeDropdown /></div>
+         <div class="flex-item">End</div>
+         <div class="flex-item"><TimeDropdown /></div>
+         <div class="flex-item"><TimeDropdown /></div>
+         <div class="flex-item"><TimeDropdown /></div>
+         <div class="flex-item"><TimeDropdown /></div>
+         <div class="flex-item"><TimeDropdown /></div>
+         <div class="flex-item"><TimeDropdown /></div>
+         <div class="flex-item"><TimeDropdown /></div>
+         </div>{/* hard code */}
+
+      {/* <h1 className="title">Employee Availability</h1>
+      <div className="flex-container">
+        <FlexItem count={numItems} />
+      </div> */} {/* attempt at recursion */}
+    
+    </div>
+
+  );
+}
+
+// function FlexItem({ count }) {
+//     if (count === 0) {
+//       return null;
+//     }
+  
+//     return (
+//       <div className="flex-item">
+//         {/* Text content within the flex item */}
+//         <p>Box {count}</p>
+//         <FlexItem count={count - 1} />
+//       </div>
+//     );
+//   } w/ recursion
+
+export default EmpAvail;
