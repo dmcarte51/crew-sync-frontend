d436b94afbea8f916b925865c30e9f6ab098f5cc
jeremybivans <jeremybivans@gmail.com>
2023-11-01 15:05:37 -0600

    Calendar tests

---
+++ src/components/tests/Calendar.test.js
@@ -0,0 +1,45 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import Calendar from '../Calendar.js';
+
+jest.mock('./EmpWorkWeek', () => {
+    return function DummyEmpWorkWeek() {
+      return <div data-testid="mock-empworkweek"></div>;
+    };
+});
+
+describe('Calendar', () => {
+
+    it('renders without crashing', () => {
+        render(<Calendar />);
+    });
+    
+    it('renders without empData', () => {
+        render(<Calendar />);
+    });
+      
+    it('renders the correct days of the week', () => {
+        const daysOfWeek = ['', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        const { getByText } = render(<Calendar daysOfWeek={daysOfWeek} />);
+        daysOfWeek.forEach(day => {
+            const dayElement = getByText(day);
+            expect(dayElement).toBeInTheDocument();
+        });
+    });
+
+    it('renders the EmpWorkWeek component for each day', () => {
+        const empData = [
+            { day: 'Monday', shifts: [] },
+            { day: 'Tuesday', shifts: [] },
+            { day: 'Wednesday', shifts: [] },
+            { day: 'Thursday', shifts: [] },
+            { day: 'Friday', shifts: [] },
+            { day: 'Saturday', shifts: [] },
+            { day: 'Sunday', shifts: [] },
+        ];
+        const { getAllByTestId } = render(<Calendar empData={empData} />);
+        const empWorkWeeks = getAllByTestId('emp-work-week');
+        expect(empWorkWeeks).toHaveLength(7);
+    });
+    
+});
