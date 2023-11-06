091b2db7574411a117503c7587f8cfbe1887d3ff
dhicks6 <dhicks23@msudenver.edu>
2023-11-06 12:04:43 -0700

    New models added

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-  username = models.CharField(max_length=30)
-  email = models.EmailField(max_length=30)
-  password = models.CharField(max_length=30)
+# class User(models.Model):
+#   username = models.CharField(max_length=30)
+#   email = models.EmailField(max_length=30)
+#   password = models.CharField(max_length=30)
+#   first_name = models.CharField(max_length=30)
+#   last_name = models.CharField(max_length=30)
+#   phone = models.CharField(max_length=10)
+
+class employee(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
   phone = models.CharField(max_length=10)
-  
+
 class Availability(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   date = models.DateField()
